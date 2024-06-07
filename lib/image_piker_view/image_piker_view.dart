ba3f1ba1719375dff2a783644967a9e98302e3f0
mdkyder <mdkyderhossain@gmail.com>
2024-06-07 22:51:25 +0600

    image_piker_file

--- lib/image_piker_view/image_piker_view.dart
+++ lib/image_piker_view/image_piker_view.dart
@@ -1,76 +1,76 @@
-import 'dart:io';
-
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
-class ImagePickerWidget extends StatefulWidget {
-  const ImagePickerWidget({Key? key}) : super(key: key);
-
-  @override
-  State<ImagePickerWidget> createState() => _ImagePickerWidgetState();
-}
-
-class _ImagePickerWidgetState extends State<ImagePickerWidget> {
-  final ImagePicker picker = ImagePicker();
-  XFile? image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  onPressed: () async {
-                    // Pick an image from gallery.
-                    XFile? pickedImage =
-                    await picker.pickImage(source: ImageSource.gallery);
-                    setState(() {
-                      image = pickedImage;
-                    });
-                  },
-                  icon: Icon(Icons.image),
-                ),
-                IconButton(
-                  onPressed: () async {
-                    // Pick an image from camera.
-                    XFile? pickedImage =
-                    await picker.pickImage(source: ImageSource.camera);
-                    setState(() {
-                      image = pickedImage;
-                    });
-                  },
-                  icon: Icon(Icons.picture_as_pdf),
-                ),    IconButton(
-                  onPressed: () async {
-                    // Pick an image from camera.
-                    XFile? pickedImage =
-                    await picker.pickMedia();
-                    setState(() {
-                      image = pickedImage;
-                    });
-                  },
-                  icon: Icon(Icons.camera),
-                ),
-              ],
-            ),
-            if (image != null)
-              Expanded(
-                child: Image.file(File(image!.path)),
-              )
-            else
-              Expanded(
-                child: Center(
-                  child: Text('No image selected'),
-                ),
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// import 'dart:io';
+//
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+//
+// class ImagePickerWidget extends StatefulWidget {
+//   const ImagePickerWidget({Key? key}) : super(key: key);
+//
+//   @override
+//   State<ImagePickerWidget> createState() => _ImagePickerWidgetState();
+// }
+//
+// class _ImagePickerWidgetState extends State<ImagePickerWidget> {
+//   final ImagePicker picker = ImagePicker();
+//   XFile? image;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Container(
+//         child: Column(
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 IconButton(
+//                   onPressed: () async {
+//                     // Pick an image from gallery.
+//                     XFile? pickedImage =
+//                     await picker.pickImage(source: ImageSource.gallery);
+//                     setState(() {
+//                       image = pickedImage;
+//                     });
+//                   },
+//                   icon: Icon(Icons.image),
+//                 ),
+//                 IconButton(
+//                   onPressed: () async {
+//                     // Pick an image from camera.
+//                     XFile? pickedImage =
+//                     await picker.pickImage(source: ImageSource.camera);
+//                     setState(() {
+//                       image = pickedImage;
+//                     });
+//                   },
+//                   icon: Icon(Icons.picture_as_pdf),
+//                 ),    IconButton(
+//                   onPressed: () async {
+//                     // Pick an image from camera.
+//                     XFile? pickedImage =
+//                     await picker.pickMedia();
+//                     setState(() {
+//                       image = pickedImage;
+//                     });
+//                   },
+//                   icon: Icon(Icons.camera),
+//                 ),
+//               ],
+//             ),
+//             if (image != null)
+//               Expanded(
+//                 child: Image.file(File(image!.path)),
+//               )
+//             else
+//               Expanded(
+//                 child: Center(
+//                   child: Text('No image selected'),
+//                 ),
+//               ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
